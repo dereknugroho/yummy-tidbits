2403abd670d3436d385ed1b22e508e0e72ab6931
Derek <derek@Delight.local>
2020-04-20 16:32:32 -0400

    Improve comments

--- itoa_base.c
+++ itoa_base.c
@@ -1,28 +1,11 @@
 /*
-
-################################################################################
-
-This is a program that converts a number to a string. Given a value and a base,
-this program displays the given value in terms of the specified base.
-
-For example:
-
-If value is 93126 and base is 14, the program will output 25d1c.
-
-Note: There is no malloc error handling.
-
-################################################################################
-
-Usage:
-$ gcc -Wall -Wextra -Werror itoa_base.c -o ./itoa_base
-$ ./itoa_base
-  Choose a strictly positive value: 93126
-  Choose any base from 2 up to 16: 14
-  The representation of the value 93126 in base 14 is: 25d1c
-$
-
-################################################################################
-
+**
+**  This program converts a number to a string in a specified base.
+**
+**  Usage:
+**  $ gcc -Wall -Wextra -Werror itoa_base.c -o ./itoa_base
+**  $ ./itoa_base
+**
 */
 
 #include <stdio.h>
@@ -36,11 +19,13 @@ int			main(void)
 	int		base = 10;
 	char	*result = "";
 
+  // Ask for a value
 	printf("Enter a strictly positive value: ");
 	scanf("%d", &value);
 	if (value <= 0)
 		return (printf("Invalid value! Terminating program.\n"));
 
+  // Ask for a base
 	printf("Enter any base between 2 and 16 (inclusive): ");
 	scanf("%d", &base);
 	if (base < 2 || base > 16)
@@ -48,6 +33,7 @@ int			main(void)
 
 	result = itoa_base(value, base);
 
+  // Display the result
 	printf("The representation of the value %d in base %d is: %s\n", value, base, result);
 	return (0);
 }
@@ -65,7 +51,7 @@ char	*itoa_base(int value, int base)
 		num /= base;
 	}
 	
-	// Set some vars
+  // Allocate memory
 	num = value;
 	ret = (char *)malloc(sizeof(char) * (bytes + 1));
 
